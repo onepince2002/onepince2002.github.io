3cb3737af01513629912424bd9bba367789d23a9
eli-wang <abc26829470@yahoo.com.tw>
2023-11-09 12:48:10 +0800

    feat: init

--- pic/test.ps1
+++ pic/test.ps1
@@ -1,7 +1,7 @@
 # 把所有的 JPG 檔案重新命名為 image_編號.jpg
 Get-ChildItem -Path "*" -Include '*.png','*.jpg' -Exclude '*image*' | ForEach-Object -Begin {
 # Get-ChildItem '*.jpg' | ForEach-Object -Begin {
-  $count = 19
+  $count = 35
 } -Process {
   Rename-Item $_ -NewName "image_$count.jpg"
   $count++
@@ -11,3 +11,4 @@ Get-ChildItem -Path "*" -Include '*.png','*.jpg' -Exclude '*image*' | ForEach-Ob
 Get-ChildItem *.jpeg | Rename-Item -NewName {
   [System.IO.Path]::ChangeExtension($_.Name, ".jpg")
 }
+
